b5f083fd18089570e084e28d9afdb3e922d6203d
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2022-09-17 23:35:47 -0400

    Clean up Home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
