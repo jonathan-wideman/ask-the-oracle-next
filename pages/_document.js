61fb97ca54a636ff7ab9a9350a426fa2b134d493
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2022-09-20 00:32:30 -0400

    Add optimized google font

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Caudex:wght@400;700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+
