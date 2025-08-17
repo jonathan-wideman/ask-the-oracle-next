6ead0c7114065dd417e8fa1fe0c021357843c392
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2025-08-17 04:58:23 -0400

    WIP basic move oracles

--- pages/moves/index.js
+++ pages/moves/index.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import Layout from "../../components/Layout";
-import { getMoves } from "../../lib/connector";
+import { getMoves, getOracles } from "../../lib/connector";
 import { classNames, styleAnimationDelay, toTitleCase } from "../../lib/util";
 import utilityStyles from "../../styles/utility.module.css";
 import oracleStyles from "../../styles/Oracle.module.css";
 import Markdown from "react-markdown";
-import { useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { useDiceContext } from "../../contexts/DiceContext";
 
-export default function Moves({ moves }) {
+export default function Moves({ moves, oracles }) {
   const movesByCategory = moves.reduce(
     (acc, move) => ({
       ...acc,
@@ -50,6 +51,7 @@ export default function Moves({ moves }) {
                       move={move}
                       index={index}
                       allMoveNames={allMoveNamesWithoutAsterisks}
+                      oracles={oracles}
                     />
                   </li>
                 ))}
@@ -59,6 +61,112 @@ export default function Moves({ moves }) {
         </div>
       </main>
     </Layout>
+  );
+}
+
+function MoveOracle({ children, oracles }) {
+  const matches = children.match(/ORACLE:(.*)/);
+  const oracleTitle = matches[1];
+  // TODO: clean this up
+  function formatTitle(title) {
+    // trim the index from the raw title
+    const trimmed = title.replace(/^.*: /, "");
+    // convert to title case
+    return toTitleCase(trimmed);
+  }
+  function createSlug(title) {
+    // based on https://www.30secondsofcode.org/js/s/slugify
+    return formatTitle(title)
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "")
+      .replace(/[\s_-]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+  }
+  const slug = createSlug(oracleTitle);
+  const oracle = oracles.find((oracle) => oracle.slug === slug);
+
+  const [tableVisible, setTableVisible] = useState(false);
+
+  const [result, setResult] = useState("Seek your fate...");
+  const { roll } = useDiceContext();
+
+  const [rolling, setRolling] = useState(false);
+  const timeoutRef = useRef(null);
+  const resetAnimation = (delay) => {
+    timeoutRef.current = setTimeout(() => setRolling(false), delay);
+  };
+  useEffect(() => {
+    // Clear the timeout interval when the component unmounts
+    return () => clearTimeout(timeoutRef.current);
+  }, []);
+
+  const rollOracle = (delay = 10) => {
+    setRolling(true);
+    resetAnimation(delay);
+    setResult(roll(oracle));
+  };
+
+  // useEffect(() => {
+  //   rollOracle(50);
+  // }, []);
+
+  const toggleTable = () => setTableVisible((visible) => !visible);
+
+  return (
+    <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+      {/* <div
+        style={{
+          color: "magenta",
+          fontFamily: '"Cinzel", serif',
+          fontWeight: "bold",
+        }}
+      >
+        {children}
+      </div>
+      <div>title: {oracleTitle}</div>
+      <div>slug: {slug}</div>
+      <div>id: {oracle?.id}</div> */}
+      <div>
+        <button
+          onClick={() => rollOracle()}
+          className={classNames(
+            oracleStyles.result,
+            rolling ? utilityStyles.transparent : utilityStyles.fadein
+          )}
+        >
+          {result}
+        </button>
+      </div>
+
+      {tableVisible ? (
+        <div className={classNames(oracleStyles.table, utilityStyles.fadein)}>
+          {oracle.table.map((row, index) => (
+            <Fragment key={index}>
+              <span
+                className={utilityStyles.fadein}
+                style={styleAnimationDelay(index * 0.025 + 0.1)}
+              >
+                {row.roll}
+              </span>
+              <span
+                className={utilityStyles.fadein}
+                style={styleAnimationDelay(index * 0.025 + 0.1)}
+              >
+                {row.result}
+              </span>
+            </Fragment>
+          ))}
+        </div>
+      ) : null}
+      <button
+        onClick={() => toggleTable()}
+        className={utilityStyles.fadein}
+        style={styleAnimationDelay(0.5)}
+      >
+        {tableVisible ? "put away" : "consult"} the runic charts
+      </button>
+    </div>
   );
 }
 
@@ -107,17 +215,79 @@ const specialTextRegexPerElements = {
     // 1 experience
     {
       regex: /^ORACLE:.*$/,
-      component: ({ children }) => (
-        <span
-          style={{
-            color: "magenta",
-            fontFamily: '"Cinzel", serif',
-            fontWeight: "bold",
-          }}
-        >
-          {children}
-        </span>
-      ),
+      // component: ({ children, oracles }) => {
+      //   const matches = children.match(/ORACLE:(.*)/);
+      //   const oracleTitle = matches[1];
+      //   // TODO: clean this up
+      //   function formatTitle(title) {
+      //     // trim the index from the raw title
+      //     const trimmed = title.replace(/^.*: /, "");
+      //     // convert to title case
+      //     return toTitleCase(trimmed);
+      //   }
+      //   function createSlug(title) {
+      //     // based on https://www.30secondsofcode.org/js/s/slugify
+      //     return formatTitle(title)
+      //       .toLowerCase()
+      //       .trim()
+      //       .replace(/[^\w\s-]/g, "")
+      //       .replace(/[\s_-]+/g, "-")
+      //       .replace(/^-+|-+$/g, "");
+      //   }
+      //   const slug = createSlug(oracleTitle);
+      //   const oracle = oracles.find((oracle) => oracle.slug === slug);
+      //   const tableVisible = true;
+
+      //   return (
+      //     <>
+      //       <div
+      //         style={{
+      //           color: "magenta",
+      //           fontFamily: '"Cinzel", serif',
+      //           fontWeight: "bold",
+      //         }}
+      //       >
+      //         {children}
+      //       </div>
+      //       <div>title: {oracleTitle}</div>
+      //       <div>slug: {slug}</div>
+      //       <div>id: {oracle?.id}</div>
+      //       <button
+      //         onClick={() => rollOracle()}
+      //         className={classNames(
+      //           oracleStyles.result,
+      //           rolling ? utilityStyles.transparent : utilityStyles.fadein
+      //         )}
+      //       >
+      //         {result}
+      //       </button>
+
+      //       {tableVisible ? (
+      //         <div
+      //           className={classNames(oracleStyles.table, utilityStyles.fadein)}
+      //         >
+      //           {oracle.table.map((row, index) => (
+      //             <Fragment key={index}>
+      //               <span
+      //                 className={utilityStyles.fadein}
+      //                 style={styleAnimationDelay(index * 0.025 + 0.25)}
+      //               >
+      //                 {row.roll}
+      //               </span>
+      //               <span
+      //                 className={utilityStyles.fadein}
+      //                 style={styleAnimationDelay(index * 0.025 + 0.25)}
+      //               >
+      //                 {row.result}
+      //               </span>
+      //             </Fragment>
+      //           ))}
+      //         </div>
+      //       ) : null}
+      //     </>
+      //   );
+      // },
+      component: MoveOracle,
     },
   ],
   strong: [
@@ -137,7 +307,7 @@ const specialTextRegexPerElements = {
   ],
 };
 
-export function Move({ move, index, allMoveNames }) {
+export function Move({ move, index, allMoveNames, oracles }) {
   const [open, setOpen] = useState(false);
   return (
     <>
@@ -191,7 +361,7 @@ export function Move({ move, index, allMoveNames }) {
                 );
                 const replacementComponent = matches[0]?.component;
                 if (replacementComponent) {
-                  return replacementComponent(rest);
+                  return replacementComponent({ ...rest, oracles });
                 }
                 return (
                   <span
@@ -215,5 +385,5 @@ export function Move({ move, index, allMoveNames }) {
 }
 
 export async function getStaticProps() {
-  return { props: { moves: getMoves() } };
+  return { props: { moves: getMoves(), oracles: getOracles() } };
 }
