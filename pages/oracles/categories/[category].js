d8a1e9d762008894db80ccb9aea8e2a6fbdb4fd9
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2025-08-28 20:35:10 -0400

    Add oracle categories routes

---
+++ pages/oracles/categories/[category].js
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import Layout from "../../../components/Layout";
+import {
+  getOraclesCategories,
+  getOraclesListings,
+} from "../../../lib/connector";
+import {
+  classNames,
+  styleAnimationDelay,
+  toTitleCase,
+} from "../../../lib/util";
+import utilityStyles from "../../../styles/utility.module.css";
+import oracleStyles from "../../../styles/Oracle.module.css";
+
+export default function Oracles({ oracles }) {
+  const oraclesByCategory = oracles.reduce(
+    (acc, oracle) => ({
+      ...acc,
+      [oracle.category]: [...(acc[oracle.category] || []), oracle],
+    }),
+    {}
+  );
+
+  return (
+    <Layout pageTitle={"Oracles"}>
+      <main>
+        <div
+          className={classNames(
+            utilityStyles.container,
+            utilityStyles.content_center
+          )}
+        >
+          <p
+            className={classNames(oracleStyles.text_xxl, utilityStyles.fadein)}
+          >
+            What do you seek?
+          </p>
+          {Object.keys(oraclesByCategory).map((category, index) => (
+            <div
+              key={category}
+              className={classNames(oracleStyles.text_xl, utilityStyles.fadein)}
+              style={styleAnimationDelay(index * 0.025 + 0.25)}
+            >
+              <p className={oracleStyles.heading_margin}>
+                {toTitleCase(category)}
+              </p>
+              <ul>
+                {oraclesByCategory[category].map((oracle, index) => (
+                  <li key={oracle.id}>
+                    <Link href={`/oracles/${oracle.slug}`}>
+                      <a
+                        className={classNames(
+                          oracleStyles.text_l,
+                          utilityStyles.fadein
+                        )}
+                        style={styleAnimationDelay(index * 0.025 + 0.25)}
+                      >
+                        {oracle.title}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getOraclesCategories().map((category) => ({
+    params: { category },
+  }));
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { category } = context.params;
+  console.log("category", category);
+  return { props: { oracles: getOraclesListings(category) } };
+}
---
+++ pages/oracles/categories/index.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Layout from "../../../components/Layout";
+import { getOraclesCategories } from "../../../lib/connector";
+import {
+  classNames,
+  styleAnimationDelay,
+  toTitleCase,
+} from "../../../lib/util";
+import utilityStyles from "../../../styles/utility.module.css";
+import oracleStyles from "../../../styles/Oracle.module.css";
+
+export default function Oracles({ categories }) {
+  return (
+    <Layout pageTitle={"Oracles"}>
+      <main>
+        <div
+          className={classNames(
+            utilityStyles.container,
+            utilityStyles.content_center
+          )}
+        >
+          <p
+            className={classNames(oracleStyles.text_xxl, utilityStyles.fadein)}
+          >
+            What do you seek?
+          </p>
+          {categories.map((category, index) => (
+            <div
+              key={category}
+              className={classNames(oracleStyles.text_xl, utilityStyles.fadein)}
+              style={styleAnimationDelay(index * 0.025 + 0.25)}
+            >
+              <p className={oracleStyles.heading_margin}>
+                <Link href={`/oracles/categories/${category}`}>
+                  {toTitleCase(category)}
+                </Link>
+              </p>
+            </div>
+          ))}
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  return { props: { categories: getOraclesCategories() } };
+}
