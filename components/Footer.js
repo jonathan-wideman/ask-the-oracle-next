f34c3f931ca4f9c890fb2b25939e36630e08e199
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2025-09-18 22:09:46 -0400

    Footer bg

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { Container } from "./atoms/Container";
 
 export default function Footer() {
   return (
-    <footer className="absolute right-0 left-0 bottom-0">
+    <footer className="absolute right-0 left-0 bottom-0 bg-zinc-800">
       <Container>
         <div className="flex flex-col items-center text-center text-sm p-4">
           <p>©2025 Jonathan Wideman</p>
