71aea3042894cbc295c1c5141e60077c9f34b58f
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2025-09-22 20:42:07 -0400

    Alternate background images

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="bg-zinc-900 text-zinc-200 font-body bg-[url('/knotwork-tiling-dkmode.png')] bg-center bg-size-[160px] bg-repeat bg-blend-multiply">
+      <body className="bg-zinc-900 text-zinc-200 font-body bg-[url('/knotwork-tiling-dkmode-00.png')] bg-center bg-size-[160px] bg-repeat bg-blend-multiply">
         <Main />
         <NextScript />
       </body>
