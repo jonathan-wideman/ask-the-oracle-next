0b3a56c9ab8aef1d97afbc4ef56237ff36484356
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2025-11-09 20:07:04 -0500

    Fix code tag default not displaying children

--- components/markdown/MoveCodeTagDefault.tsx
+++ components/markdown/MoveCodeTagDefault.tsx
@@ -1,7 +1,6 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export const MoveCodeTagDefault = ({
-  children,
   ...rest
 }: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>) => (
   <span className="font-code text-indigo-400 font-bold" {...rest} />
